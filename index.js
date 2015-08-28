6ee136b75280c7f99f45081496020a93f5f8099d
inei <iancnei@gmail.com>
2015-08-28 16:15:36 -0700

    create a local array of objects of testing routes

--- index.js
+++ index.js
@@ -10,11 +10,21 @@ app.use("/vendor", express.static("bower_components"));
 
 var views = path.join(process.cwd(), "views");
 
+var phrase = [
+{phrase: "Ukemi", description: "The term used when a person catches themself on the ground before fully hitting the ground.  It is commonly referred to as 'recovering' in the english speaking community."},
+{phrase: "Setplay", description: "The term used for a set of commands practiced beforehand that can lead to a multitude of different variations, frequently used when referring to a situation where a character does something when another cahracter is recovering from a knocked down state.  It is commonly referred to as 'setups' in the english speaking community."},
+{phrase: "Oki", description: "The term used for the situation when a character is recovering from a knocked down state.  It is commonly referred to as 'wakeup' in the english speaking community."}
+];
+
 app.get("/", function (req, res) {
-  var homePath = path.join(views, "home.html");
-  res.sendFile(homePath);
+  	var homePath = path.join(views, "home.html");
+  	res.sendFile(homePath);
+});
+
+app.get("/phrases", function(req, res){
+	res.send(phrase);
 });
 
 app.listen(3000, function () {
-  console.log("WORKING");
+	console.log("WORKING");
 });
